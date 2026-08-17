ca437f1b39341120a2870710f0284c58b60c7b90
negbiaviv-del <negbiaviv@gmail.com>
2026-08-17 07:02:29 +0000

    Secure backend with non-root user and fix Gunicorn permissions

--- Backend/entrypoint.sh
+++ Backend/entrypoint.sh
@@ -3,6 +3,6 @@
 echo "Initializing Database..."
 python3 setup_db.py
 
-# 2. אם הסקריפט הצליח, מריצים את השרת (ה-Gunicorn)
-echo "Starting Gunicorn..."
-exec gunicorn --bind 0.0.0.0:5000 app:app
+# 2. קריאת הפקודה המלאה מה-Dockerfile (עם כל פרמטרי האבטחה)
+echo "Starting Application..."
+exec "$@"
